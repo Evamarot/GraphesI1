d7393f91ea48f43a809f9e68aac1fbea2d82aa36
Sacha Portal <sacha.portal@efrei.net>
2025-02-28 17:28:49 +0100

    Création Matrice + affichage de la matrice + amélioration affichage tableau de contraintes

--- Graphes.py
+++ Graphes.py
@@ -1,16 +1,31 @@
+from idlelib.configdialog import is_int
+
+
 class Graphes:
     def __init__(self):
-        # Liste des attributs de l'automate
+        # Liste des attributs du graphe
         self.tache = [0]
         self.duree = [0]
         self.contraintes = [[]]
+
+        # Matrice d'adjacence
+
+        # COMMENTAIRES A SUPPRIMER :
+        # self.matrice a la même forme que dans l'exemple donné : si des tâches sont
+        # des contraintes, alors elle est ajoutée dans le tableau, sinon une étoile est rajoutée.
+        #
+        # Cela veut dire que si une tâche est une contrainte, alors la case contient une valeur numérique
+        # plutôt qu'une chaine de caractère. On peut donc facilement filtrer en faisant des is_int(x).
+
+        self.matrice = []
+
 
     def ajout_omega(self):
         """Ajoute un état oméga à l'automate"""
         self.tache.append(len(self.tache))
         self.duree.append(0)
         self.contraintes.append(list())
-        # Rajouter la seule tâche qui n'est la contrainte d'aucune autre tâche
+        # Rajouter les tâches qui ne sont les contraintes d'aucune autre tâche
         for t in self.tache[1:-1]:
             for c in self.contraintes:
                 if t in c:
@@ -28,7 +43,6 @@ class Graphes:
                 if i != len(text) - 1: ligne = ligne[:-1].split(" ")
                 else: ligne = ligne.split(" ")
 
-
                 self.tache.append(int(ligne[0]))
                 self.duree.append(int(ligne[1]))
 
@@ -39,9 +53,50 @@ class Graphes:
 
     def afficher_contraintes(self):
         """Affiche le tableau avec les tâches, les durées et les prédécessseurs"""
-        print(f"{'Tâche':<10}{'Durée':<10}{'Prédécesseurs':<20}")
+        print("-" * 44)
+        print(f"|{'Tâche':^10}|{'Durée':^10}|{'Prédécesseurs':^20}|")
         for i in range(1, len(self.tache)):
-            print(f"{self.tache[i]:<10}{self.duree[i]:<10}{', '.join(map(str, self.contraintes[i])):<20}")
+            print(f"|{self.tache[i]:^10}|{self.duree[i]:^10}|{', '.join(map(str, self.contraintes[i])):^20}|")
+        print("-" * 44 + "\n")
+
+    def creer_matrice(self):
+        """Crée une matrice d'adjacence à partir des contraintes"""
+
+        # Création de la matrice de taille (n+1) x (n+1) avec des étoiles
+        self.matrice = [["*" for _ in range(len(self.tache))] for _ in range(len(self.tache))]
+
+        # Remplissage de la matrice avec les durées des contraintes
+        for i in range(1, len(self.tache)):
+            for j in self.contraintes[i]:
+                self.matrice[j][i] = self.duree[j]
+
+    def afficher_matrice(self):
+        """Affiche la matrice des durées des contraintes"""
+        print("Matrice des durées des contraintes :")
+
+        # Ligne supérieure du tableau
+        print("-" * 9 + "-" * 6 * len(self.tache))
+
+        # Ligne des tâches et des labels
+        print(f"|{'Tâche':^7}|", end="")
+        for i in range(len(self.tache)):
+            print(f"{i:^5}|", end="")
+
+        # Ligne de séparation entre les tâches et les valeurs
+        print("\n|-------" + "".join(["-" * 6] * len(self.tache)) + "|")
+
+        # Affichage des lignes des valeurs
+        for i in range(len(self.tache)):
+            # Colonne des tâches
+            print(f"|{self.tache[i]:^7}", end="|")
+
+            # Cellules des valeurs de la ligne
+            for j in range(len(self.tache)):
+                print(f"{self.matrice[i][j]:^5}", end="|")
+            print()
+
+        # Ligne inférieure du tableau
+        print("-" * 9 + "-" * 6 * len(self.tache) + "\n")
 
 if __name__ == "__main__":
     g = Graphes()
@@ -50,11 +105,15 @@ if __name__ == "__main__":
     print("Avant l'ajout de l'oméga :")
     print(g.tache)
     print(g.duree)
-    print(g.contraintes)
+    print(g.contraintes, end="\n\n")
     g.afficher_contraintes()
 
     print("\nAprès l'ajout de l'oméga :")
     g.ajout_omega()
     print(g.tache)
     print(g.duree)
-    print(g.contraintes)
+    print(g.contraintes, end="\n\n")
+    g.afficher_contraintes()
+
+    g.creer_matrice()
+    g.afficher_matrice()
